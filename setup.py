c8322b809ec1f3a9258b9270cdbef4a2dfdeb5b7
Vasyliy I. Gurianov <vg2007sns@rambler.ru>
2020-03-05 10:42:53 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+import pymms
+
+setup(
+    name='pymms',
+    version=pymms.__version__,
+    description='Simulation of the Minkowski metric  for Python',
+    license='MIT',
+    author='Vasyliy I. Gurianov',
+    author_email='vg2007sns@rambler.ru',
+    url='https://vgurianov.github.io/srt/mms/',    
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'matplotlib >=1.4.3'
+    ],
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    test_suite='test',
+    python_requires='==2.7',
+)
