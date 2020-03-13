d5723fedd6e3bc14798f04470f59eb7cd4892b50
Vasyliy I. Gurianov <vg2007sns@rambler.ru>
2020-03-13 11:51:07 +0300

    Update test_simple.py

--- test_simple.py
+++ test_simple.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from pymms import mms
 from pymms import research_instruments as ri
+from pymms import drawing
+from pymms import print_results
 
 
 class PyMmsTestCase(TestCase):
@@ -8,4 +10,6 @@ class PyMmsTestCase(TestCase):
     def test_run(self):
         observer = ri.Table()
         xt= mms.Composite( 10, 1, observer)
+        pr = print_results.print_results(dp)
+        visio = drawing.Visualization(dp)
         self.assertEqual(xt.runtest(), 'Test mms')
