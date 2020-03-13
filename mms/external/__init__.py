47a3d854df8745201afb361602397d1677192f5b
Vasyliy I. Gurianov <vg2007sns@rambler.ru>
2020-03-13 10:09:28 +0300

    Delete __init__.py

--- mms/external/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Special Reletivity Software
-Srt is a software package that can be used to simulate of the Minkovski metric. It uses UML2 SP language to non-numeric modeling. 
-"""
-__all__ = ["prettytable"]
