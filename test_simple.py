1e8ea37861d82e2ad3526a55e64ffcc230d4defb
Vasyliy I. Gurianov <vg2007sns@rambler.ru>
2020-03-14 11:45:32 +0300

    Update test_simple.py

--- test_simple.py
+++ test_simple.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from pymms import mms
+from pymms import mms_ex
 from pymms import research_instruments as ri
 from pymms import drawing
 from pymms import print_results
@@ -9,7 +10,8 @@ class PyMmsTestCase(TestCase):
     
     def test_run(self):
         observer = ri.Table()
-        xt= mms.Composite( 10, 1, observer)
+        xt= mms.Composite( 10, 5, observer)
+        xt_ex= mms_ex.Composite( 10, 5, observer,5)
         dp = ri.DataProcessing(observer, 5, 10, 10)
         pr = print_results.TablePrint(dp)
         visio = drawing.Visualization(dp)
