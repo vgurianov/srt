363cdd4d088466a34ae22b2efe209118cd23b40c
Vasyliy I. Gurianov <vg2007sns@rambler.ru>
2020-04-03 11:21:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     license='MIT',
     author='Vasyliy I. Gurianov',
     author_email='vg2007sns@rambler.ru',
-    url='https://vgurianov.github.io/srt/pymms/',    
+    url='https://vgurianov.github.io/srt',    
     packages=find_packages(exclude=['experiments']),
     install_requires=[
         'matplotlib ==2.2.4'
